57cf3a69f39036544a54d048f3c802cc8dc2fdca
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2025-04-04 18:28:33 +0800

    Added test example for EFC

---
+++ EntityFrameworkCore/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EntityFrameworkCore.Entities;
+
+namespace EntityFrameworkCore
+{
+    class AppContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; } = null!;
+
+        public AppContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Sourse=sqlite.db");
+        }
+    }
+}
---
+++ EntityFrameworkCore/Program.cs
@@ -0,0 +1,32 @@
+﻿using EntityFrameworkCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace EntityFrameworkCore
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (AppContext db = new())
+            {
+                Student student1 = new Student { Name = "Vasya", Age = 20 };
+                Student student2 = new Student { Name = "Petya", Age = 35 };
+
+                db.Add(student1);
+                db.Add(student2);
+                db.SaveChanges();
+
+                Console.WriteLine("Students added");
+
+                var students = db.Students.ToList();
+
+                foreach (var student in students)
+                {
+                    Console.WriteLine($"[{student.StudentId}] {student.Name}, {student.Age}");
+                }
+            }
+        }
+    }
+}
